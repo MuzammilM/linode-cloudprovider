43a879101e6270ddd75a82c54c58065bb2a8255f
Muzammil Hasan Momin <muzammilmomin@live.com>
2019-01-23 15:21:47 +0530

    Update provisionInstance.sh

--- provisionInstance.sh
+++ provisionInstance.sh
@@ -12,18 +12,25 @@ pip install linode-cli --upgrade
 echo "${ERROR}Please configure the cli first ${reset}"
 exit
 fi
+function listStackScripts(){
+token=$1
+user=$2
+echo "User : "$user
+curl -s "https://api.linode.com/v4/linode/stackscripts" -H 'Authorization: Bearer '$token'' -H 'X-Filter: { "username": "'$user'"}' | jq '[.data[] | {id:.id,label:.label,description:.description}]'
+}
 
 function createLinode(){
 grp=$1
 echo "Use Linode StackScripts ?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) read -erp"Enter scriptid " scriptid ; ip=$(linode-cli linodes create --root_pass --region ap-south --image $linImage --group $grp --type $linType --authorized_keys "`cat ~/.ssh/id_rsa.pub`" --label $label --no-header --stackscript_id $scriptid --format 'ipv4' | awk '{print $2}'); break;;
+        Yes ) echo "Available scripts :" ; read -esrp"Enter LINODE TOKEN " TOKEN ; read -erp"Enter LINODE username " USERNAME; listStackScripts $TOKEN $USERNAME ; read -erp"Enter scriptid " scriptid ; ip=$(linode-cli linodes create --root_pass --region ap-south --image $linImage --group $grp --type $linType --authorized_keys "`cat ~/.ssh/id_rsa.pub`" --label $label --no-header --stackscript_id $scriptid --format 'ipv4' | awk '{print $2}'); break;;
         No ) ip=$(linode-cli linodes create --root_pass --region ap-south --image $linImage --group $grp --type $linType --authorized_keys "`cat ~/.ssh/id_rsa.pub`" --label $label --no-header --format 'ipv4' | awk '{print $2}'); break;;
     esac
 done
 ip2=`echo $ip | sed -e 's/ //g'`
 sudo sed  -i '/\['"$grp"'\]/a '"$label"' ansible_ssh_host='"$ip2"'' /etc/ansible/hosts
+echo -e "Host "$label"\nUser root\nHostname "$ip2 >> .ssh/config
 echo "New machine ip :"$ip2
 }
 read -erp"Enter ansible/linode group name(Eg: Spark) " gname
